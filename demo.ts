49fae379584fec5ba327cdf35ced0d7fa12aece6
Katie Lin <katielin019@gmail.com>
2024-03-26 16:34:03 -0400

    first commit

---
+++ demo.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+async function makeOneGetRequest(requestNumber: number) {
+    // const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    const response = await axios.get('https://rest-example.covey.town');
+    console.log(`Request ${requestNumber} completed, server replied: `, response.data);
+}
+
+async function makeThreeSimpleRequests() {
+    makeOneGetRequest(1);
+    makeOneGetRequest(2);
+    makeOneGetRequest(3);
+}
+
+makeThreeSimpleRequests();
+
+async function makeThreeSerialRequests(): Promise<void> {
+}
